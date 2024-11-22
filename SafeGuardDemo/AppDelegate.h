87a938d74e9ad7ccff9b5e19c99cd45a96634729
JohnJin007 <2219566118@qq.com>
2024-11-22 17:32:22 +0800

    Initial Commit

---
+++ SafeGuardDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SafeGuardDemo
+//
+//  Created by v_jinlilili on 2024/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SafeGuardDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  SafeGuardDemo
+//
+//  Created by v_jinlilili on 2024/11/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
